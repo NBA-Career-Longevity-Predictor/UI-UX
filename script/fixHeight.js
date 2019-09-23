d79e058b1bdcbcea7e0900a6814238e5606c903e
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-09-23 17:45:15 -0500

    working landing page

---
+++ script/fixHeight.js
@@ -0,0 +1,7 @@
+// var header = document.getElementById("myHeader");
+// var hero = document.getElementById("myHero");
+
+// var heightVar = document.getElementById('myHero').style.getPropertyValue('height');
+// console.log(heightVar);
+
+// header.setAttribute('style','height:'+heightVar+';');
