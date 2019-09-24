8083816a56a147d3bac311dca62424ba524dfaa7
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-09-24 10:39:24 -0500

    Sticky nav + sticky title working on tablet and mobile

--- script/fixHeight.js
+++
@@ -1,7 +0,0 @@
-// var header = document.getElementById("myHeader");
-// var hero = document.getElementById("myHero");
-
-// var heightVar = document.getElementById('myHero').style.getPropertyValue('height');
-// console.log(heightVar);
-
-// header.setAttribute('style','height:'+heightVar+';');
