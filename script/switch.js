bfd13beed31bc171b407796152d731bd78fe1221
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-09-24 13:59:11 -0500

    fix typos and dark mode icon margins

--- script/switch.js
+++ script/switch.js
@@ -2,4 +2,4 @@ function toggleDarkLight() {
     var body = document.getElementById("body");
     var currentClass = body.className;
     body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-  }
+}
