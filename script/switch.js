0a04fd1cc6e90eb57a1d70d2906e90c061f1a4fd
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-09-25 00:11:52 -0500

    dark mode toggle comments, need to clean the function's logic

--- script/switch.js
+++ script/switch.js
@@ -1,19 +1,25 @@
-var body = document.getElementById("body");
-var currentClass = body.className;
-console.log()
+
+var body = document.getElementById("body"); //find body ID on page
+var currentClass = body.className; //assign it to currentClass variable
+
+// if toggle from session storage isn't null, assign the value to currentClass
 if(sessionStorage.getItem("toggle") != null){
     currentClass = sessionStorage.getItem("toggle");
 }
+// otherwise assign it to our default "light-mode"
 else{
     sessionStorage.setItem("toggle", "light-mode");
 }
+// reassigns the body className to value after check when the page loads
 body.className = currentClass;
 
-console.log(`Page loaded. className is ${currentClass}.`);
+// console.log(`Page loaded. className is ${currentClass}.`);
 
+// toggles dark mode on and off from a button in navbar.
 function toggleDarkLight() {
     console.log(`Before switching the toggle value is: ${sessionStorage.getItem("toggle")}`);
 
+    //CLEAN UP THIS CODE'S REDUNDANCY
     if (sessionStorage.getItem("toggle") === "light-mode") {
         body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
         sessionStorage.setItem("toggle", "dark-mode");
@@ -28,6 +34,6 @@ function toggleDarkLight() {
         currentClass = body.className;
     }
 
-    console.log(`Current class is: ${currentClass}`);
-    console.log(`After switching the toggle value is: ${sessionStorage.getItem("toggle")}`);
+    // console.log(`Current class is: ${currentClass}`);
+    // console.log(`After switching the toggle value is: ${sessionStorage.getItem("toggle")}`);
 }
